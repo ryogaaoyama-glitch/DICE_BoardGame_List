2f0311f56804e18475fc958082826c4c852f1dc2
ryogaaoyama-glitch <ryoga.aoyama@keio.jp>
2026-06-08 20:10:30 +0900

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  const params = new URLSearchParams(req.query).toString();
+  const url = `https://script.google.com/macros/s/AKfycbxthxssVkUWIkWVaurUtAzI7MukRESiYPKaErDC4sEIYW-nnKfIBK-_IvbgSMOYAP_s/exec?${params}`;
+  
+  const response = await fetch(url, { redirect: 'follow' });
+  const text = await response.text();
+  
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'text/javascript');
+  res.status(200).send(text);
+}
